e6623bfe79865800779b30f4342eef8d46063e13
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2023-01-09 12:31:20 +0700

    table modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Form from './Form';
 import './index.css';
-import List from './List';
+import Table from './Table';
 
 function App() {
 
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <Form reqType={reqType} setReqType={setReqType} />
-      <List items={items} />
+      <Table items={items} />
     </div>
   );
 }
